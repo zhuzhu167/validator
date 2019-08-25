f6b3b0686e607104da68328c73af47f4facdc793
focus <1678881243@qq.com>
2019-08-25 20:22:26 +0800

    注释修正

--- js/bootstrapCheck.js
+++ js/bootstrapCheck.js
@@ -1,23 +1,22 @@
+// 告诉 javascript 引擎这是一个函数表达式，不是函数声明
 (function (global, factory, plug) {
-	// 告诉javascript引擎这是一个函数表达式，不是函数声明
-	return factory.call(global, global.jQuery, plug); // call 把 factory 的内容劫持到了 global 上，并传入两个参数分别是jq对象和插件名
+	// call 把 factory 的内容劫持到了 global 上，并传入两个参数分别是 jq 对象和插件名
+	return factory.call(global, global.jQuery, plug);
 })(typeof window !== undefined ? window : this, function ($, plug) {
-	// $.fn.method()=function(){}的调用把方法扩展到了对象的prototype上
+	// $.fn.method()=function(){} 的调用把方法扩展到了对象的 prototype 上
 	var __DFFS__ = {
 		raise: "change"
 	};
-
 	var __RULES__ = {
 		"require": function () {
 			return !!this.val();
 		}
 	};
-
+	// 在 jq 对象的 prototype 上添加一个方法
 	$.fn[plug] = function (ops) {
 		this.each(function () {
 			var $this = $(this);
-			// console.log($this)
-			// 合并 $this 和 ops，修改并返回 $this。
+			// 合并 $this 和 ops，修改并返回 $this
 			$.extend($this, ops);
 			$this.raise = $this.data("bv-raise") || $this.raise || __DEFF__.raise;
 			var $field = $this.find("[data-bv=true]");
